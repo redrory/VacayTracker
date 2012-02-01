4168aa6a2377387f7584b80e44e6c156de080847
Rory Walker <redrory@gmail.com>
2012-01-31 20:24:10 -0500

    JS update

--- app/models/vacay.rb
+++ app/models/vacay.rb
@@ -3,6 +3,8 @@ class Vacay < ActiveRecord::Base
 	attr_accessible :brought_forward, :dateFrom, :dateUntil, :user_id, :sumVacay, :employee_id
 	belongs_to :user
 
+	validates_presence_of :dateFrom, :message => "Please enter starting date"
+
 	#validates_presence_of :dateFrom, :on => :create, :message => "Please enter value"
 
 	#validates_presence_of :dateUntil, :on => :create, :message => "Please enter value"
